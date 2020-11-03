d7e30e168fe0336c2f7c38519b09594aa08510c8
Zaiyan Alam <alamm@usc.edu>
2020-11-03 04:53:16 -0800

    Image URL Loader, Google Sheets to Unity
    Completed Image URL loader, used Scalar images.
    Stated working on Google Sheets to Unity script.

---
+++ Assets/Client.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Client : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    private Sprite targetSprite;
+    public string url ;
+    private void Start()
+    {
+        StartCoroutine(GetTextureRequest(url, (response) => {
+            targetSprite = response;
+            spriteRenderer.sprite = targetSprite;
+        }));
+    } 
+
+    IEnumerator GetTextureRequest(string url, System.Action<Sprite> callback)
+    {
+        using (var www = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                if (www.isDone)
+                {
+                    var texture = DownloadHandlerTexture.GetContent(www);
+                    var rect = new Rect(0, 0, texture.width, texture.height);
+                    var sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+                    callback(sprite);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SheetsImporter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+
+using System.Security.Cryptography.X509Certificates;
+
+using UnityEngine;
+using UnityEditor;
+
+class LevelSynchronizer
+{
+    const string spreadsheetId = "1sakdfj0392029482_sajfalsdfhlsadkfsak";
+    const string sheetNameAndRange = "MySheet"; //You can also put a cell range here
+    const string p12PathFromAsset = "Plugins/example-23948239f.p12";
+
+    public static void SyncLevel()
+    {
+        String serviceAccountEmail = "example@myproject.iam.gserviceaccount.com";
+
+        var certificate = new X509Certificate2(Application.dataPath + Path.DirectorySeparatorChar + p12PathFromAsset, "notasecret", X509KeyStorageFlags.Exportable);
+
+        ServiceAccountCredential credential = new ServiceAccountCredential(
+           new ServiceAccountCredential.Initializer(serviceAccountEmail)
+           {
+               Scopes = new[] { SheetsService.Scope.SpreadsheetsReadonly }
+               /*
+                Without this scope, it will :
+                GoogleApiException: Google.Apis.Requests.RequestError
+                Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential.
+                lol..
+                */
+           }.FromCertificate(certificate));
+
+
+        var service = new SheetsService(new BaseClientService.Initializer()
+        {
+            HttpClientInitializer = credential,
+        });
+
+        SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, sheetNameAndRange);
+
+        StringBuilder sb = new StringBuilder();
+
+        ValueRange response = request.Execute();
+        IList<IList<object>> values = response.Values;
+        if (values != null && values.Count > 0)
+        {
+            foreach (IList<object> row in values)
+            {
+                foreach (object cell in row)
+                {
+                    sb.Append(cell.ToString() + " ");
+                }
+                //Concat the whole row
+                Debug.Log(sb.ToString());
+                sb.Clear();
+            }
+        }
+        else
+        {
+            Debug.Log("No data found.");
+        }
+    }
+}
